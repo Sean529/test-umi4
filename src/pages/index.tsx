1decf7221038f8344cdebf591c92802068fc9cea
AT <atxiesz@gmail.com>
2022-11-10 16:20:29 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,9 @@
-import yayJpg from '../assets/yay.jpg';
+import './index.less'
 
 export default function HomePage() {
   return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
+    <div className="toast btn-primary">
+      hello world
     </div>
-  );
+  )
 }
