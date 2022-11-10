4d4d98dfdc807e82796df0d5bb2f08d148298022
AT <atxiesz@gmail.com>
2022-11-10 16:54:38 +0800

    toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import { showToast } from '@/utils'
-import './index.less'
 
 export default function HomePage() {
   showToast('hello world')
   return (
-    <div className="toast btn-primary">
-      hello world
-    </div>
+    <div></div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 module.exports = {
-  content: [
-    './src/pages/**/*.tsx',
-    './src/components/**.tsx',
-    './src/layouts/**.tsx',
-    './src/*.ts',
-    './src/utils.ts'
-  ],
+  content: ['./src/pages/**/*.tsx', './src/components/**.tsx', './src/layouts/**.tsx', 
+  './src/utils.ts'],
+  theme: {
+    fontFamily: {
+      'b-Bold': 'Barlow-Bold',
+      'b-Medium': 'Barlow-Medium',
+      'b-Regular': 'Barlow-Regular',
+      'b-SemiBold': 'Barlow-SemiBold',
+      'b-Black': 'Barlow-Black',
+      'b-ExtraBold': 'Barlow-ExtraBold',
+      'bv-SemiBold': 'BeVietnamPro-SemiBold',
+      'bv-Regular': 'BeVietnamPro-Regular',
+      'bv-Bold': 'BeVietnamPro-Bold',
+    },
+    keyframes: {
+      screwdriver: {
+        '0%': { bottom: 0 },
+        '100%': { bottom: '94px' },
+      },
+    },
+    animation: {
+      toast: 'screwdriver 0.5s ease-in-out forwards',
+    },
+  },
+  plugins: [],
 }
