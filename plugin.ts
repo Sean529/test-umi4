6dab9a4566923f06dc678e34048ecccf858134c1
AT <atxiesz@gmail.com>
2022-11-22 14:58:39 +0800

    feat: 支持 safe

--- plugin.ts
+++ plugin.ts
@@ -3,7 +3,7 @@ import type { IApi } from "umi"
 export default (api: IApi) => {
   // viewport cover
   api.addHTMLMetas(() => ({
-    content: "viewport",
-    name: "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover",
+    name: "viewport",
+    content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover",
   }))
 }
