3af634fdcfad9826ef725f01fce6b54e9ee322af
AT <atxiesz@gmail.com>
2022-12-02 18:33:28 +0800

    feat: metas 配置

--- .umirc.ts
+++ .umirc.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "umi"
-const px2rem = require('postcss-plugin-px2rem')
+const px2rem = require("postcss-plugin-px2rem")
 
 export default defineConfig({
+  metas: [{ name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover" }],
   extraPostCSSPlugins: [
     require("tailwindcss")(),
     "autoprefixer",
