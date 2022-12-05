0eff3d459a8b4f61f8d4e825fc2b8a1467384f88
AT <atxiesz@gmail.com>
2022-12-05 10:51:56 +0800

    feat: test react-vant

--- .umirc.ts
+++ .umirc.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "umi"
 const px2rem = require("postcss-plugin-px2rem")
 
 export default defineConfig({
-  metas: [{ name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover" }],
   extraPostCSSPlugins: [
     require("tailwindcss")(),
-    "autoprefixer",
     px2rem({
       rootValue: 100, // 根据设计稿750设置
-      exclude: [/node_modules\/react-vant/i],
+      exclude: [/node_modules\/react-vant/i]
+    }),
+    px2rem({
+      rootValue: 50, // 根据设计稿375设置
+      exclude: [/^(?!.*node_modules\/react-vant)/]
     }),
   ],
 })
